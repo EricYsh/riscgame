861a8dcf742c77bc8a4bf87715ca80e3485e6347
Eric Yin <ericyin@Erics-MacBook-Pro-5.local>
2023-04-05 22:47:13 +0800

    add comments

--- controller/src/main/java/edu/duke/ece651/riscgame/game/Territory.java
+++ controller/src/main/java/edu/duke/ece651/riscgame/game/Territory.java
@@ -2,16 +2,27 @@ package edu.duke.ece651.riscgame.game;
 
 import java.util.HashSet;
 
+/**
+ * The Territory class represents a territory in the RISC game.
+ * Each territory has a name, an owner, a number of units, and a set of neighboring territories.
+ */
 public class Territory {
     private final String name;
     private int ownId;
     private int unitNum;
     private HashSet<Territory> neighbors;
 
+    /**
+     * Constructor for the Territory class.
+     *
+     * @param tname The name of the territory.
+     */
     public Territory(String tname) {
         this.name = tname;
         neighbors = new HashSet<>();
     }
+
+    // Getters
 
     public String getName() {
         return name;
@@ -29,6 +40,8 @@ public class Territory {
         return neighbors;
     }
 
+    // Setters
+
     public void setOwnId(int oid) {
         ownId = oid;
     }
@@ -37,18 +50,41 @@ public class Territory {
         unitNum = num;
     }
 
+    // Other methods
+
+    /**
+     * Adds a number of units to the territory.
+     *
+     * @param num The number of units to add.
+     */
     public void addUnit(int num) {
         unitNum += num;
     }
 
+    /**
+     * Removes a number of units from the territory.
+     *
+     * @param num The number of units to remove.
+     */
     public void minusUnit(int num) {
         unitNum -= num;
     }
 
+    /**
+     * Adds a neighboring territory to the territory's set of neighbors.
+     *
+     * @param t1 The neighboring territory to add.
+     */
     public void addNeighbor(Territory t1) {
         neighbors.add(t1);
     }
 
+    /**
+     * Generates a formatted string with information about the territory, including
+     * the number of units, the territory name, and the names of neighboring territories.
+     *
+     * @return A formatted string with information about the territory.
+     */
     public String displayInfo() {
         StringBuilder info = new StringBuilder("");
         String format = unitNum + " units in " + name + " (next to: ";
