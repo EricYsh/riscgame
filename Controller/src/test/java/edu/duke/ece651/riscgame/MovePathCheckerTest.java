8dd499ec38c98e8eb75aa0d2aeec2cbc78a1995a
Evangeline <jw844@duke.edu>
2023-04-05 22:12:59 +0800

    add more tests

--- Controller/src/test/java/edu/duke/ece651/riscgame/MovePathCheckerTest.java
+++ Controller/src/test/java/edu/duke/ece651/riscgame/MovePathCheckerTest.java
@@ -45,10 +45,23 @@ public class MovePathCheckerTest {
         Territory t5 = new Territory("F", 2, 5);
         Territory t6 = new Territory("G", 1, 5);
         t1.addNeighbor(t2);
+        t1.addNeighbor(t3);
         t2.addNeighbor(t1);
-        t2.addNeighbor(t3);
-        t3.addNeighbor(t2);
+        t2.addNeighbor(t4);
+        t3.addNeighbor(t1);
+        t3.addNeighbor(t4);
+        t3.addNeighbor(t5);
+        t4.addNeighbor(t2);
+        t4.addNeighbor(t3);
+        t4.addNeighbor(t6);
+        t5.addNeighbor(t3);
+        t5.addNeighbor(t6);
+        t6.addNeighbor(t5);
+        t6.addNeighbor(t4);
+
         MovePathChecker movePath = new MovePathChecker(null);
-        assertFalse(movePath.ifPathExist(t1, t3));
+        assertFalse(movePath.ifPathExist(t1, t6));
+        assertTrue(movePath.ifPathExist(t1, t3));
+
     }
 }
