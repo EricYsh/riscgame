93fa416a9e36ac6ffff4be3fad891cb58addcdd0
Your Name Here <yongchang.liu@duke.edu>
2023-04-15 00:11:53 +0800

    add a class to generalize send and receive object, currently generalize send object in netServer

--- Server/src/main/java/edu/duke/ece651/riscgame/ReceiveUnitAssignmentThread.java
+++ Server/src/main/java/edu/duke/ece651/riscgame/ReceiveUnitAssignmentThread.java
@@ -1,5 +1,6 @@
 package edu.duke.ece651.riscgame;
 
+import edu.duke.ece651.riscgame.commuMedium.GameMessageStream;
 import edu.duke.ece651.riscgame.commuMedium.ValidationResult;
 import edu.duke.ece651.riscgame.game.Territory;
 import edu.duke.ece651.riscgame.rule.InputRuleChecker;
@@ -8,7 +9,6 @@ import java.net.Socket;
 import java.util.Vector;
 
 import static edu.duke.ece651.riscgame.NetServer.receiveUnitAssignment;
-import static edu.duke.ece651.riscgame.NetServer.sendIllegalOrder;
 
 public class ReceiveUnitAssignmentThread extends SocketThread <Vector<Territory> >{
     int numUnit;
@@ -22,8 +22,7 @@ public class ReceiveUnitAssignmentThread extends SocketThread <Vector<Territory>
             Vector<Territory> terrVec = receiveUnitAssignment(socket);
             System.out.println("receive one assignment");
             String check = new InputRuleChecker<>().checkMyRule(terrVec, numUnit);
-            System.out.println(check);
-            sendIllegalOrder(socket, new ValidationResult(check, false));
+            GameMessageStream.sendObject(new ValidationResult(check, false), socket);
             if (check == null) {
                 System.out.println("receive valid assignment");
                 return terrVec;
